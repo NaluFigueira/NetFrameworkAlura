131fa80e3053d0f58e2072948697189622566f6a
NaluFigueira <anachfigueira@gmail.com>
2022-01-30 01:25:15 -0300

    Initializing project

---
+++ 1_MoviesAPI/1_MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using MoviesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
