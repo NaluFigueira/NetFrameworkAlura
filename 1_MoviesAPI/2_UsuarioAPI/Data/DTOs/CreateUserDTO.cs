f273cfe1ebac072a110979dcd2e0bb6cd61876c5
NaluFigueira <anachfigueira@gmail.com>
2022-03-05 00:36:11 -0300

    Creating user api

---
+++ 1_MoviesAPI/2_UsuarioAPI/Data/DTOs/CreateUserDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _2_UserAPI.Data.DTOs
+{
+    public class CreateUserDTO
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+    }
+}
---
+++ 1_MoviesAPI/2_UsuarioAPI/Profiles/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using _2_UserAPI.Data.DTOs;
+using _2_UserAPI.Models;
+using AutoMapper;
+
+namespace _2_UserAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<CreateUserDTO, User>();
+        }
+    }
+}
