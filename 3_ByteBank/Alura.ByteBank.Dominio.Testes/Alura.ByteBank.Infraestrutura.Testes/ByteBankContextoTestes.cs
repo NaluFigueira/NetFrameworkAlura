b142e35fcf03b3e854175ad0be7429623b117ec1
NaluFigueira <anachfigueira@gmail.com>
2022-06-03 18:02:38 -0300

    Integration tests

---
+++ 3_ByteBank/Alura.ByteBank.Dominio.Testes/Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTestes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Alura.ByteBank.Dados.Contexto;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBankContextoTestes
+    {
+        [Fact]
+        public void TestaConexaoContextoComMySQL()
+        {
+            //Arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+
+            //Act
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch
+            {
+                throw new Exception("Não foi poss[ivel conectar com a base de dados.");
+            }
+
+            //Assert
+            Assert.True(conectado);
+        }
+
+    }
+}
